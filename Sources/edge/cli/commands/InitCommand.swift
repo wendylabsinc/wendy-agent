0d10931819f09f91b7827c6a98a95e7fd8b79cfd
Joannis Orlandos <joannis@orlandos.nl>
2025-05-03 11:16:36 +0200

    Add the `edge init` command

---
+++ Sources/edge/cli/commands/InitCommand.swift
@@ -0,0 +1,16 @@
+import ArgumentParser
+import Shell
+
+struct InitCommand: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "init",
+        abstract: "Initialize a new EdgeOS project in the current directory."
+    )
+    
+    func run() async throws {
+        print("Initializing new EdgeOS project...")
+        try await Shell.run([
+            "swift", "package", "init", "--type", "executable"
+        ])
+    }
+}
