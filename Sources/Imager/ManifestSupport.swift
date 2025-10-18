77515b3a5ed545d9e9292b8de48cab5b1222c7f3
Joannis Orlandos <joannis@orlandos.nl>
2025-10-18 11:23:28 +0200

    Import FoundationNetworking

--- Sources/Imager/ManifestSupport.swift
+++ Sources/Imager/ManifestSupport.swift
@@ -2,6 +2,9 @@ import AsyncHTTPClient
 import Foundation
 import NIOCore
 import NIOFoundationCompat
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 // MARK: - Data Models
 
