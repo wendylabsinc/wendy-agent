83162083cb55c185aa9376e31f7c0c331748bb6e
Joannis Orlandos <joannis@orlandos.nl>
2025-10-22 17:38:37 +0200

    Extra logging

--- Scripts/codesign.sh
+++ Scripts/codesign.sh
@@ -9,11 +9,14 @@ EXECUTABLE_PATH=".build/release/wendy"
 CODESIGN_IDENTITY="Developer ID Application: Wendy Labs Inc. (3YVC792H3S)"
 
 # Codesign the executable
+echo "Codesigning executable at $EXECUTABLE_PATH..."
 codesign --force --options runtime --sign "$CODESIGN_IDENTITY" "$EXECUTABLE_PATH"
 
 # Zip the executable
+echo "Zipping executable..."
 ditto -c -k --keepParent "$EXECUTABLE_PATH" "$EXECUTABLE_PATH.zip"
 
+echo "Submitting executable for notarization..."
 xcrun notarytool submit "$EXECUTABLE_PATH.zip" --keychain-profile notary-profile --wait
 
 echo "Executable signed and notarized successfully."
